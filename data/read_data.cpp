acdadb7d6cd78023cd068f26248c2612967c086f
Ayazor <91690216+Ayazor@users.noreply.github.com>
2025-03-28 16:13:51 +0300

    дополнительная работа с данными на C++
    добавил два файла, которые работают с данными с роботов и экспортят их в csv, чтобы потом работать с ними на питоне

---
+++ data/read_data.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string file_path = "MRCLAM_Dataset1/Robot1_Groundtruth.dat";
+
+    std::ifstream file(file_path, std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cerr << "Ошибка: файл не найден! Проверьте путь: " << file_path << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    file.close();
+    return 0;
+}
